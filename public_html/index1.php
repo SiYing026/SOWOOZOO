038aac6dc60b2a2a84d5e731a04d9342aff7b3fa
SiYing026 <87138208+SiYing026@users.noreply.github.com>
2022-07-21 19:41:51 +0800

    gallery

--- public_html/index1.php
+++ public_html/index1.php
@@ -195,7 +195,87 @@
 
     <!-- about section ends -->
 
-   
+   <!-- gallery section starts  -->
+
+   <section class="gallery" id="gallery">
+
+        <h1 class="heading">Traveler Collection</h1>
+        
+    <div class="slideshow-container">
+
+        <div class="mySlides fade">
+          <img src="images/collection_1.jpg" style="width:100%">
+          <div class="desp"><strong>KL Tower</strong><br>The 7<sup>th</sup> tallest telecommunications tower in the world</div>
+        </div>
+
+        <div class="mySlides fade">
+          <img src="images/collection_2.jpg" style="width:100%">
+          <div class="desp"><strong>Kuala Lumpur City Centre</strong><br> The world's tallest twin skyscrapers</div>
+        </div>
+        
+        <div class="mySlides fade">
+          <img src="images/collection_3.jpg" style="width:100%">
+          <div class="desp"><strong>Independence Square</strong><br> The Union Flag was lowered and the Malaysian flag hoisted for the first time</div>
+          
+        </div>
+
+        <div class="mySlides fade">
+          <img src="images/collection_4.jpg" style="width:100%">
+          <div class="desp"><strong>Malaysia National Palace</strong><br> The official residence of the Yang di-Pertuan Agong</div>
+        </div>
+        
+        <div class="mySlides fade">
+          <img src="images/collection_5.jpg" style="width:100%">
+          <div class="desp"><strong>Kuala Lumpur Environment</strong></div>
+        </div>
+        
+        <div class="mySlides fade">
+          <img src="images/collection_6.jpg" style="width:100%">
+          <div class="desp"><strong>Kuala Lumpur Environment</strong></div>
+        </div>
+        
+        <div class="mySlides fade">
+          <img src="images/collection_7.jpg" style="width:100%">
+          <div class="desp"><strong>Kuala Lumpur Environment</strong></div>
+        </div>
+
+    </div>
+    <br>
+
+    <div style="text-align:center">
+      <span class="dot"></span> 
+      <span class="dot"></span> 
+      <span class="dot"></span>
+      <span class="dot"></span> 
+      <span class="dot"></span> 
+      <span class="dot"></span> 
+      <span class="dot"></span> 
+    </div>
+
+    <script>
+        let slideIndex = 0;
+        showSlides();
+
+        function showSlides() {
+          let i;
+          let slides = document.getElementsByClassName("mySlides");
+          let dots = document.getElementsByClassName("dot");
+          for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+          }
+          slideIndex++;
+          if (slideIndex > slides.length) {slideIndex = 1}    
+          for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+          }
+          slides[slideIndex-1].style.display = "block";  
+          dots[slideIndex-1].className += " active";
+          setTimeout(showSlides, 2800); // Change image every 2 seconds
+        }
+    </script>
+    </section>
+<!-- gallery section ends -->
+
 
 <!-- packages section starts  -->
     <section class="packages" id="packages">
